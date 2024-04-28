ea50b6b204c4669f6b46ca4223cfc1b23aa195d1
baker.lee <ljw722007@gmail.com>
2024-04-28 13:23:47 +0900

    gui

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QVBoxLayout, QMessageBox
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QLineEdit, QPushButton, QHBoxLayout, QVBoxLayout, QMessageBox
 import pyautogui
 import keyboard
 import time
@@ -9,35 +9,44 @@ class ScreenCaptureApp(QWidget):
     def __init__(self):
         super().__init__()
 
+        self.repetitions_label = None
         self.repetitions = None
         self.stop_flag = None
         self.stop_button = None
         self.start_button = None
         self.repetitions_entry = None
-        self.repetitions_label = None
 
         self.init_ui()
 
     def init_ui(self):
         self.setWindowTitle("Screen Capture App")
 
+        # Total Page 입력 칸
         self.repetitions_label = QLabel("Total Page:")
         self.repetitions_entry = QLineEdit()
+        repetitions_layout = QHBoxLayout()
+        repetitions_layout.addWidget(self.repetitions_label)
+        repetitions_layout.addWidget(self.repetitions_entry)
+
+        # Start와 Stop 버튼
         self.start_button = QPushButton("Start")
         self.stop_button = QPushButton("Stop")
-        self.stop_button.setEnabled(False)
+        buttons_layout = QHBoxLayout()
+        buttons_layout.addWidget(self.start_button)
+        buttons_layout.addWidget(self.stop_button)
 
+        # 전체 레이아웃
         layout = QVBoxLayout()
-        layout.addWidget(self.repetitions_label)
-        layout.addWidget(self.repetitions_entry)
-        layout.addWidget(self.start_button)
-        layout.addWidget(self.stop_button)
+        layout.addLayout(repetitions_layout)
+        layout.addLayout(buttons_layout)
 
         self.setLayout(layout)
 
+        # 버튼에 클릭 이벤트 연결
         self.start_button.clicked.connect(self.start_capture)
         self.stop_button.clicked.connect(self.stop_capture)
 
+        # 초기화
         self.stop_flag = False
         self.repetitions = 0
 
@@ -53,11 +62,7 @@ class ScreenCaptureApp(QWidget):
             msg.exec_()
             return
 
-        self.start_button.setEnabled(False)
-        self.stop_button.setEnabled(True)
-        self.repetitions_entry.setEnabled(False)
-
-        time.sleep(1)
+        time.sleep(3)
         pyautogui.press('enter')
 
         for _ in range(self.repetitions):
@@ -70,10 +75,6 @@ class ScreenCaptureApp(QWidget):
             time.sleep(0.5)
             keyboard.press_and_release('right')
             time.sleep(0.3)
-
-        self.stop_button.setEnabled(False)
-        self.start_button.setEnabled(True)
-        self.repetitions_entry.setEnabled(True)
 
     def stop_capture(self):
         self.stop_flag = True
